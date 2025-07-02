625d18600c417781a5c004f613c6fc3ca66f2d10
Sergey-8057 <erste_sergey@proton.me>
2025-07-03 02:32:33 +0300

    add file SearchBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
-// import css from './App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
+import css from './App.module.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
+    <div className={css.app}>
+      <SearchBar />
     </div>
   );
 }
